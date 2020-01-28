b861ac13aea7ca1411794ffc1c4b95a53a2ff137
Ryan Bachrach <ryanbachrach9@gmail.com>
2020-01-28 10:42:56 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by ryan bachrach on 1/28/20.
+//  Copyright © 2020 Novak Software Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
