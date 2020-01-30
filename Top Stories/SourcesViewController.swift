853979d5b56946e74ab9bbd9f0021f51433479ca
Ryan Bachrach <ryanbachrach9@gmail.com>
2020-01-30 10:36:01 -0600

    finished initial set up of SourcesTableView, no errors

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -50,9 +50,10 @@ class SourcesViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return sources.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell( withIdentifier: "Cell", for: indexPath)
-        let source = sources(indexPath.row)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = sources[indexPath.row]
         cell.textLabel?.text = source["name"]
         cell.detailTextLabel?.text = source["descrition"]
         return cell
